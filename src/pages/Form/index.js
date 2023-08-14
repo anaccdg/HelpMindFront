2892b41ec4cf4977eaf9f3ed97093158a31f99d5
Ana Carla Callegarim Della Giacomo <74308573+acdgiacomo@users.noreply.github.com>
2023-08-14 20:57:13 -0300

    Revert "Merge pagina formulario"

--- src/pages/Form/index.js
+++ src/pages/Form/index.js
@@ -1,97 +1,33 @@
-import { useEffect, useState } from 'react';
-import React from 'react';
-import logo from "../../images/logo.png";
-import dog1 from "../../images/form/dog1.png";
-import dog2 from "../../images/form/dog2.png";
-import dog3 from "../../images/form/dog3.png";
-import cat1 from "../../images/form/cat1.png";
-import cat2 from "../../images/form/cat2.png";
-import cat3 from "../../images/form/cat3.png";
-import { TextField, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
-import './form.css';
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import './form.css'
+import Logo from "../../images/logo.png"
+import Descricao from "../../images/descricao.png"
 
-function Form() {
-    const [avatar, setAvatar] = useState('');
-
-    const handleAvatarChange = (event, newAvatar) => {
-        setAvatar(newAvatar);
-    };
-
-    return (
-        <div>
-            <div className="header">
-                <div className="content">
-                    <div className="text-container">
-                        <h1 className="title">Bom te ver por aqui! :)</h1>
-                        <br />
-                        <h2 className="subtitle">O primeiro passo é preencher esse formulário.</h2>
-                    </div>
-                    <div className="logo-container">
-                        <img src={logo} alt="Logo" className="logo" />
-                    </div>
-                </div>
-            </div>
-            <div className="form-container">
-                <h2 className="form-title">Suas informações</h2>
-                <div className="form-line"></div>
-                <div className="form-inputs">
-                    <TextField
-                        label="Apelido"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                    />
-                    <TextField
-                        label="Idade"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                    />
-                    <TextField
-                        label="Gênero"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                    />
-                    <div className="avatar-wrapper">
-                        <div className="avatar-title MuiInputLabel-root">Avatar</div>
-                        <ToggleButtonGroup
-                            value={avatar}
-                            exclusive
-                            onChange={handleAvatarChange}
-                            className="avatar-toggle-group"
-                        >
-                            <ToggleButton value="dog1">
-                                <img src={dog1} alt="Dog 1" className="avatar-image" />
-                            </ToggleButton>
-                            <ToggleButton value="dog2">
-                                <img src={dog2} alt="Dog 2" className="avatar-image" />
-                            </ToggleButton>
-                            <ToggleButton value="dog3">
-                                <img src={dog3} alt="Dog 3" className="avatar-image" />
-                            </ToggleButton>
-                            <ToggleButton value="cat1">
-                                <img src={cat1} alt="Cat 1" className="avatar-image" />
-                            </ToggleButton>
-                            <ToggleButton value="cat2">
-                                <img src={cat2} alt="Cat 2" className="avatar-image" />
-                            </ToggleButton>
-                            <ToggleButton value="cat3">
-                                <img src={cat3} alt="Cat 3" className="avatar-image" />
-                            </ToggleButton>
-                        </ToggleButtonGroup>
-                    </div>
-                    <div className="button-container">
-                        <Link className="button-navegation" to="/Chat">
-                            Conversar
-                        </Link>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    );
+function Form(){
+        return(      
+            <div className='header'>               
+                <img className='logo' src={Logo}/>                  
+                <div className='texto-descritivo'>
+                    <h1>Bom te ver por aqui!</h1>
+                    <h3>O primeiro passo, é preencher esse formulário:</h3>
+                </div>                                
+                <div className='background'>
+                    <form className='formulario'>
+                        <div>
+                            <input classname='apelido' type='text' placeholder='Insira seu apelido' />
+                        </div>
+                        <div>
+                            <input classname='idade' type='number' placeholder='Insira sua idade' />
+                        </div>
+                        <div>
+                            <input classname='genero' type='text' placeholder='Insira seu gênero' />
+                        </div>
+                    </form>                    
+                    <Link className='button-navegation' to={`/Chat`}>Continuar</Link>
+                </div>                
+            </div>            
+    )
 }
 
 export default Form;
